6980106118fb34bd29a6447e2a9e37339268d550
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-05-25 18:36:20 +0530

    New feature added

--- src/components/HeroHead.jsx
+++ src/components/HeroHead.jsx
@@ -1,7 +1,23 @@
+import { IoChevronBackCircle } from "react-icons/io5";
+import { IoChevronForwardCircle } from "react-icons/io5";
+import { IoArrowDownCircleOutline } from "react-icons/io5";
+import { FaRegBell } from "react-icons/fa6";
+
 export default function HeroHead() {
     return (
         <div className="hero_head">
-            
+            <div className="hero_head_top p-3 d-flex justify-content-between align-items-center">
+                <div>
+                    <IoChevronBackCircle className="nav-icons"/>
+                    <IoChevronForwardCircle className="nav-icons"/>
+                </div>
+                <div className="">
+                    <span className="nav-btns px-3 py-1 rounded-pill">Explore Premium</span>
+                    <span className="nav-btns px-3 py-1 rounded-pill ms-2"><IoArrowDownCircleOutline />Install App</span>
+                    <FaRegBell className="nav-btns p-2 rounded-circle ms-2" />
+                    <span className="nav-btns p-2 rounded-circle ms-2" >Z</span>
+                </div>
+            </div>
         </div>
     )
 }
