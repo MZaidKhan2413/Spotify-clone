d39f2ea3315e192070d4eb1ab803db6bf5e3f52d
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-05-26 09:25:45 +0530

    minor change

--- src/utils/convertTime.js
+++ src/utils/convertTime.js
@@ -1,6 +1,6 @@
 const convertTime = (time)=>{
     const min = Math.floor((time % (60 * 60 * 1000))/(60 * 1000));
-    const sec = Math.floor(min/1000);
+    const sec = Math.floor((time % (60 * 1000))/1000);
     return `${min} : ${sec}`;
 }
 
