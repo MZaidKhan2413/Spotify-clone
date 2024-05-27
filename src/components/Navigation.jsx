fb0ba70f46d30ae4f3cf6b6f4df8c65e33f149d3
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-05-27 18:31:00 +0530

    Updated navigations

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,20 +1,59 @@
+import { useState, useContext } from 'react';
 import { MdHomeFilled } from "react-icons/md";
 import { IoIosSearch } from "react-icons/io";
+import { TokenContext } from "../contexts/TokenContext";
+import { SpotifyContext } from "../contexts/SpotifyContext";
+import axios from "axios"
 
 export default function Navigation() {
-  const navs = [
-    { value: "Home", link: "/", logo: <MdHomeFilled className="fs-2 me-3"/> },
-    { value: "Search", link: "/", logo: <IoIosSearch className="fs-2 me-3"/> },
-  ];
+  const {token} = useContext(TokenContext);
+  const {setMusicData} = useContext(SpotifyContext);
+
+  const [navs, setNavs] = useState([
+    { value: "Home", isActive: true, logo: <MdHomeFilled className="fs-2 me-3"/> },
+    { value: "Search", isActive: false, logo: <IoIosSearch className="fs-2 me-3"/> },
+  ]);
+
+  const handleOnClick = (clickedNav) => {
+    const updatedNavs = navs.map((nav) => ({
+      ...nav,
+      isActive: nav.value === clickedNav.value,
+    }));
+    setNavs(updatedNavs);
+
+    clickedNav.value === "Home" && getPlaylists()
+  };
+
+  async function getPlaylists() {
+    let response = await axios.get("https://api.spotify.com/v1/browse/featured-playlists?limit=4", {
+        headers: {
+            Authorization: "Bearer "+ token,
+        }
+    });
+    const playlists = response.data.playlists.items.map((item)=>{
+        return {
+            name: item.name,
+            id: item.id,
+            tracksURL: item.tracks.href,
+        }
+    })
+    setMusicData((prevData) => ({
+        ...prevData,
+        playlists,
+    }));
+}
 
   return (
     <nav className="navigation p-3 d-flex flex-column justify-content-evenly">
       {navs.map((nav, idx) => (
         <li className="p-1" key={idx}>
-          <a href={nav.link} className="fw-bold fs-6">
+          <button 
+            onClick={() => handleOnClick(nav)} 
+            className={nav.isActive ? "fw-bold fs-6 active" : "fw-bold fs-6"}
+          >
             {nav.logo}
             {nav.value}
-          </a>
+          </button>
         </li>
       ))}
     </nav>
